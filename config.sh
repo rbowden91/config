d3c8b66ed9ae2016c149b9905f86c6f028d44c4b
Rob Bowden <rbowden91@gmail.com>
2013-09-02 12:35:13 -0400

    add git push config

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@ sudo yum -y install make ruby ruby-devel vim memcached
 
 git config --global user.name "Rob Bowden"
 git config --global user.email "rbowden91@gmail.com"
+git config --global push.default simple
 
 mkdir ~/.vim
 git clone git@github.com:rbowden91/vimrc.git ~/.vim
