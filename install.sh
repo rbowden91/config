d991792c5b2e0b99de668e584e4bf45dc65032e4
Rob Bowden <rbowden91@gmail.com>
2018-10-11 01:29:07 -0400

    fix ssh-ident stuff

--- install.sh
+++ install.sh
@@ -15,16 +15,25 @@ wget -O ~/bin/dropbox 'https://www.dropbox.com/download?dl=packages/dropbox.py'
 chmod 700 ~/bin/dropbox
 dropbox autostart y
 
-# install TeamViewer
+# spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install -y spotify-client
+
+# TeamViewer
 cd /tmp && wget https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc
 sudo apt-key add TeamViewer2017.asc
 sudo sh -c 'echo "deb http://linux.teamviewer.com/deb stable main" >> /etc/apt/sources.list.d/teamviewer.list'
 sudo sh -c 'echo "deb http://linux.teamviewer.com/deb preview main" >> /etc/apt/sources.list.d/teamviewer.list'
 sudo apt-get update
+sudo apt-get install -y teamviewer
 
-sudo apt-get install -y teamviewer chromium-browser
+# chromium
+sudo apt-get install -y chromium-browser
 xdg-settings set default-web-browser chromium-browser.desktop
 
+# TODO: hopefully files are here by now??
 cd ~/Dropbox*
 dropbox exclude add *
 dropbox exclude remove Courses grad_school Identity
@@ -47,12 +56,13 @@ cp -s ~/'Dropbox (CS50)'/Identity/ssh/id_rsa ~/.ssh/id_rsa
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/id_rsa
 
+sudo dpkg-divert --divert /usr/bin/ssh.ssh-ident --rename /usr/bin/ssh
 wget -O ~/bin/ssh-ident https://raw.githubusercontent.com/ccontavalli/ssh-ident/master/ssh-ident
-chmod 700 ~/bin/ssh-ident
-ln -s ~/bin/ssh-ident ~/bin/ssh
+sudo chmod 755 ~/bin/ssh-ident
+sudo chown root.root ~/bin/ssh-ident
+sudo mv ~/bin/ssh-ident /usr/bin/ssh
 
 cat >> ~/.bashrc << EOF
+export BINARY_SSH="/usr/bin/ssh.ssh-ident"
 alias db='cd $HOME/"Dropbox (CS50)"'
-alias scp='BINARY_SSH=scp ~/bin/ssh-ident'
-alias sftp='BINARY_SSH=scp ~/bin/ssh-ident'
 EOF
